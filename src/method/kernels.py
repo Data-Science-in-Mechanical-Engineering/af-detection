a8dd2788f55077fc9d60a510a51529339aa893d4
clathe <9933-clathe@users.noreply.git.rwth-aachen.de>
2023-07-24 22:13:34 +0200

    Implement ProductKernel.

--- src/method/kernels.py
+++ src/method/kernels.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import Callable
 
@@ -82,9 +84,52 @@ def compute_double_pairwise_kernel_matrix(
     return kernel_matrix
 
 
-class AbstractKernel(ABC):
+class BaseKernel(ABC):
+    @abstractmethod
+    def double_pairwise(self, x: np.ndarray | list[np.ndarray], y: np.ndarray | list[np.ndarray]) -> np.ndarray:
+        raise NotImplementedError
+
+    @abstractmethod
+    def double_pairwise_kme(
+            self,
+            x: np.ndarray | list[np.ndarray],
+            y: np.ndarray | list[np.ndarray],
+            batch_size: int = DEFAULT_BATCH_SIZE
+    ) -> np.ndarray:
+        raise NotImplementedError
+
+    def double_pairwise_mean(
+            self,
+            x: np.ndarray | list[np.ndarray],
+            y: np.ndarray | list[np.ndarray],
+            batch_size: int = DEFAULT_BATCH_SIZE
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    def transform(self, distances: np.ndarray | list[np.ndarray]) -> np.ndarray:
+        # TODO: specify shape
+        raise NotImplementedError
+
+    # TODO: check type hint, from python 3.10 onwards possible to delete quotation marks
+    #  check for any derived class as well
+    @abstractmethod
+    def __mul__(self, other) -> 'ProductKernel':
+        raise NotImplementedError
+
+
+class SimpleKernel(BaseKernel):
+    def __mul__(self, other) -> ProductKernel:
+        if isinstance(other, ProductKernel):
+            return ProductKernel([self] + other.kernels)
+        elif isinstance(other, SimpleKernel):
+            return ProductKernel([self] + [other])
+        else:
+            raise ValueError(f"Multiplication of {self.__class__.__name__} with {type(other)} is not defined.")
+
     def double_pairwise(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """ Computes the kernel matrix for every pair of instances and trajectories.
+        """
+        Computes the kernel matrix for every pair of instances and trajectories.
 
         For every pair of instances we compute, for every pair of their trajectories, the squared euclidean distance
         between the trajectories. The kernel transformation is then applied to that distance.
@@ -97,6 +142,7 @@ class AbstractKernel(ABC):
             The kernel matrix for every pair of instances and every pair of trajectories.
             The shape is (n_instances_x, n_instances_y, m_trajectories_x, m_trajectories_y).
         """
+
         # for every pair of instances: for every pair of trajectories: squared euclidean distance between trajectories
         # shape: (n_instances_x, n_instances_y, m_trajectories_x, m_trajectories_y)
         pairwise_distances = double_pairwise_squared_euclidean_distances(x, y)
@@ -160,12 +206,8 @@ class AbstractKernel(ABC):
 
         return compute_double_pairwise_kernel_matrix(x, y, kernel_aggregator, batch_size)
 
-    @abstractmethod
-    def transform(self, distances: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
 
-
-class RBFKernel(AbstractKernel):
+class RBFKernel(SimpleKernel):
     bandwidth: float
 
     def __init__(self, bandwidth: float):
@@ -181,3 +223,110 @@ class RBFKernel(AbstractKernel):
             The transformed distances as a measure of similarity.
         """
         return np.exp(-1 / 2 / (self.bandwidth ** 2) * distances)
+
+
+class ProductKernel(BaseKernel):
+    def __init__(self, kernels: list[BaseKernel]):
+        assert len(kernels) >= 2, f"Length of kernels expected to be at least 2. However, it is {len(kernels)}"
+        self.kernels = []
+        for kernel in kernels:
+            if isinstance(kernel, BaseKernel):
+                self.kernels.append(kernel)
+            elif isinstance(kernel, ProductKernel):
+                self.kernels.extend(kernel.kernels)
+            else:
+                raise ValueError(f"Found Kernel in kernels that is of unexpected type {type(kernel)}")
+
+    def __mul__(self, other) -> 'ProductKernel':
+        if isinstance(other, ProductKernel):
+            return ProductKernel(self.kernels + other.kernels)
+        elif isinstance(other, BaseKernel):
+            return ProductKernel(self.kernels + [other])
+        else:
+            raise ValueError(f"Multiplication of {self.__class__.__name__} with {type(other)} is not defined.")
+
+    def double_pairwise(self, x: list[np.ndarray], y: list[np.ndarray]) -> np.ndarray:
+        self._check_validity(x)
+        self._check_validity(y)
+        self._assert_equal_n_trajectories(x)
+        self._assert_equal_n_trajectories(y)
+        res = np.ones((len(x[0]), len(y[0]), x[0].shape[1], y[0].shape[1]))
+        for i, kernel in enumerate(self.kernels):
+            res *= kernel.double_pairwise(x[i], y[i])
+        return res
+
+    def double_pairwise_kme(
+            self,
+            x: list[np.ndarray],
+            y: list[np.ndarray],
+            batch_size: int = DEFAULT_BATCH_SIZE
+    ) -> np.ndarray:
+        """
+        Computes the kernel matrix as the pairwise product of the kernel matrices of each simple kernel.
+        Each single kernel matrix is the kernel matrix for every pair of instances as the KME of the trajectories.
+        Each single kernel matrix is computed in a batched fashion to limit the amount of memory used in intermediate steps.
+
+        Args:
+            x: List of features matrices [x_1, ..., x_n] where each x_i is a np.ndarray of shape (n_instances_x, m_x_i_trajectories, dim_trajcetory_i)
+            y: List of features matrices [y_1, ..., y_n] where each y_i is a np.ndarray of shape (n_instances_y, m_y_i_trajectories, dim_trajcetory_i)
+            batch_size: Matrix batch stride to use for memory-efficient computation of the kernel matrix.
+
+        Returns:
+            The kernel matrix for every pair of instances.
+            The shape is (n_instances_x, n_instances_y).
+        """
+        self._check_validity(x)
+        self._check_validity(y)
+        res = np.ones((len(x[0]), len(y[0])))
+        for i, kernel in enumerate(self.kernels):
+            res *= kernel.double_pairwise_kme(x[i], y[i], batch_size)
+        return res
+
+    def double_pairwise_mean(
+            self,
+            x: list[np.ndarray],
+            y: list[np.ndarray],
+            batch_size: int = DEFAULT_BATCH_SIZE
+    ) -> np.ndarray:
+        """
+        Computes the kernel matrix as the pairwise product of the kernel matrices of each simple kernel.
+        Each single kernel matrix is the kernel matrix for every pair of instances over the average trajectory distances.
+        Each single kernel matrix is computed in a batched fashion to limit the amount of memory used in intermediate steps.
+
+        Args:
+            x: List of features matrices [x_1, ..., x_n] where each x_i is a np.ndarray of shape (n_instances_x, m_x_i_trajectories, dim_trajcetory_i)
+            y: List of features matrices [y_1, ..., y_n] where each y_i is a np.ndarray of shape (n_instances_y, m_y_i_trajectories, dim_trajcetory_i)
+            batch_size: Matrix batch stride to use for memory-efficient computation of the kernel matrix.
+
+        Returns:
+            The kernel matrix for every pair of instances.
+            The shape is (n_instances_x, n_instances_y).
+        """
+        self._check_validity(x)
+        self._check_validity(y)
+        res = np.ones(len(x[0]), len(y[0]))
+        for i, kernel in enumerate(self.kernels):
+            res *= kernel.double_pairwise_mean(x[i], y[i], batch_size)
+        return res
+
+    def _check_validity(self, features: list[np.ndarray]):
+        assert isinstance(features, list), "Parameter must be of type list"
+        assert len(features) == len(self.kernels), (
+            f"Number of number of kernels does not match number of features: {len(features)} != {len(self.kernels)}"
+        )
+        assert all((e.shape[0] == features[0].shape[0] for e in features)), (
+            "Number of instances must be the same for all features."
+        )
+
+    @staticmethod
+    def _assert_equal_n_trajectories(x):
+        assert all((feature.shape[1] == x[0].shape[1] for feature in x)), (
+            "Number of trajectories must be the same for all features."
+        )
+
+    def transform(self, distances: list[np.ndarray]) -> np.ndarray:
+        # TODO: specify shape
+        res = self.kernels[0].transform(distances[0])
+        for i, kernel in enumerate(self.kernels[1:]):
+            res *= kernel.transform(distances[i])
+        return res
