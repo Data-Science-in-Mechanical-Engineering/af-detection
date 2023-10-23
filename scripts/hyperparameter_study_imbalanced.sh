0dc1a8506c7ba892ba9085c6934774f1fd4c0c99
Pierre-François Massiani <pfmassiani@gmail.com>
2023-10-23 15:59:23 +0200

    Change hyperparameters for grid search for imbalanced data

--- scripts/hyperparameter_study_imbalanced.sh
+++ scripts/hyperparameter_study_imbalanced.sh
@@ -3,16 +3,16 @@
 export PYTHONPATH=$PYTHONPATH:.
 
 python src/scripts/performance.py \
-    --c_lower -1 \
+    --c_lower 1 \
     --c_upper 1 \
-    --c_steps 11 \
+    --c_steps 1 \
     --rho_lower -1\
     --rho_upper 1\
     --rho_steps 20\
     --classifier KME \
-    --bandwidth_rri_lower -1 \
-    --bandwidth_rri_upper 3 \
-    --bandwidth_rri_steps 11 \
+    --bandwidth_rri_lower -2 \
+    --bandwidth_rri_upper 0 \
+    --bandwidth_rri_steps 20 \
     --bandwidth_rri_logspace \
     --bandwidth_rri_base 10.0 \
     --setup cross \
