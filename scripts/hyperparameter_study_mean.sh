86f66f69e52c4e627862c41f995b19f947914145
Pierre-François Massiani <pfmassiani@gmail.com>
2023-10-24 17:46:03 +0200

    Add script for hyperparameter study of mean

---
+++ scripts/hyperparameter_study_mean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:.
+
+python src/scripts/performance.py \
+    --c_lower 1 \
+    --c_upper 1 \
+    --c_steps 1 \
+    --rho_lower -1\
+    --rho_upper 1\
+    --rho_steps 20\
+    --classifier MEAN \
+    --bandwidth_rri_lower -2 \
+    --bandwidth_rri_upper 0 \
+    --bandwidth_rri_steps 20 \
+    --bandwidth_rri_logspace \
+    --bandwidth_rri_base 10.0 \
+    --setup cross \
+    --imbalanced_validating
