bfc2ace2e4f970188fc063116d027393f95117cd
Pierre-François Massiani <pfmassiani@gmail.com>
2023-10-30 16:31:22 +0100

    Change parameterization for balanced data set

--- scripts/test_balanced.sh
+++ scripts/test_balanced.sh
@@ -11,10 +11,10 @@ python src/scripts/performance.py \
     --rho_steps 1\
     --rho_base 0.33\
     --classifier KME \
-    --bandwidth_rri_lower -1 \
-    --bandwidth_rri_upper -1 \
+    --bandwidth_rri_lower 1 \
+    --bandwidth_rri_upper 1 \
     --bandwidth_rri_steps 1 \
     --bandwidth_rri_logspace \
-    --bandwidth_rri_base 10.0 \
+    --bandwidth_rri_base 0.05 \
     --setup cross \
     --test
